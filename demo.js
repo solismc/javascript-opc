ca5bad3907a03d1c6d2b9701d1a74000a9946623
solismc <manolocsolis@gmail.com>
2021-01-07 16:40:49 -0500

    Added Get Example

--- demo.js
+++ demo.js
@@ -32,7 +32,15 @@
     {
       get: function() {
         return  this.name.first + ' ' + this.name.last
+      },
+      set: function(value) {
+        var nameParts = value.split(' ')
+        this.name.first = nameParts[0]
+        this.name.last = nameParts[1]
       }
     })
-
+  cat.fullName = 'Muffin Top'
   console.log(cat.fullName)
+  console.log(cat.name.first)
+  console.log(cat.name.last)
+  
